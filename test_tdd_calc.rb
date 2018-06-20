4e4409afd67a164e02996f5fe64bf93153844144
Ricky Collins <richardcollins52317@gmail.com>
2018-06-20 14:14:59 -0400

    testing for subtraction

--- test_tdd_calc.rb
+++ test_tdd_calc.rb
@@ -7,14 +7,16 @@ class TestTddCalc < Minitest::Test
         assert_equal(1, 1)
     end
 
-    def test_2_plus_2
+    def test_2_plus_2_is_4
         assert_equal(4, calc_add(2, 2))
     end
 
-    def test_123_plus_987
+    def test_123_plus_987_is_1110
         assert_equal(1110, calc_add(123, 987))
     end
 
-
+    def test_5_minus_10_is_5
+        assert_equal(5, calc_sub(5, 10))
+    end
 
 end
